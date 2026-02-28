fea389926d7befc945613cdf1114420d3538cb0c
uyttt45 <ll9@ll9.de5.net>
2026-02-28 21:20:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.dnsp.dpdns.org": dockerHub,
+  ["quay.dnsp.dpdns.org": "https://quay.io",
+  ["gcr.dnsp.dpdns.org": "https://gcr.io",
+  ["k8s-gcr.dnsp.dpdns.org": "https://k8s.gcr.io",
+  ["k8s.dnsp.dpdns.org": "https://registry.k8s.io",
+  ["ghcr.dnsp.dpdns.org": "https://ghcr.io",
+  ["cloudsmith.dnsp.dpdns.org": "https://docker.cloudsmith.io",
+  ["ecr.dnsp.dpdns.org": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.dnsp.dpdns.org": dockerHub,
 };
 
 function routeByHosts(host) {
